4cfb19c8394e336fb699bfd0acca37ac45593f26
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-27 14:03:33 -0400

    [X86_64] remove unnecessary breaks

--- lib/Target/X86_64/Address.cpp
+++ lib/Target/X86_64/Address.cpp
@@ -21,19 +21,15 @@ size_t AddressDecoder::decode(unsigned char modRM)
         case 0x0:
             // disp32 when mod=00 and rm=101 (no disp otherwise)
             return (rm == 0x5) ? 4 : 0;
-            break;
         case 0x1:
             // disp8 regardless of rm value
             return 1;
-            break;
         case 0x2:
             // disp32 regardless of rm value
             return 4;
-            break;
         case 0x3:
             // no disp value
             return 0;
-            break;
         default:
             // This shouldn't be possible -- since the mask should only
             // allow for values 0-3
