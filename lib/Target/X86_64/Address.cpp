152f413ba5599e77ef1af4b144219ef8a39dd87a
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-27 13:05:14 -0400

    [X86_64] decode ModRM to get disp sizes

--- lib/Target/X86_64/Address.cpp
+++ lib/Target/X86_64/Address.cpp
@@ -1,4 +1,5 @@
 #include "Address.h"
+#include <stdexcept>
 
 using namespace tg;
 
@@ -13,10 +14,37 @@ bool AddressDecoder::hasSIB(unsigned char modRM)
 
 size_t AddressDecoder::decode(unsigned char modRM, unsigned char SIB)
 {
-    // TODO decode the ModRM and SIB to determine the required bytes
-    //  needed for addressing
-    (void)modRM;
-    (void)SIB;
+    if(hasSIB(modRM)) {
+        // TODO decode SIB
+        (void)SIB;
+        return 0;
+    }
+    else {
+        /**
+         * There is no SIB for this instruction
+         * The data in the 'SIB' variable contains random junk
+         */
 
-    return 0;
+        unsigned char mod = modRM >> 6U;
+        unsigned char rm = modRM & 0x7U;
+
+        switch (mod) {
+            case 0x0:
+                // disp32 when mod=00 and rm=101 (no disp otherwise)
+                return (rm == 0x5) ? 4 : 0;
+            case 0x1:
+                // disp8 regardless of rm value
+                return 1;
+            case 0x2:
+                // disp32 regardless of rm value
+                return 4;
+            case 0x3:
+                // no disp value
+                return 0;
+            default:
+                // This shouldn't be possible -- since the mask should only
+                // allow for values 0-3
+                throw std::runtime_error("bad value for ModRM");
+        }
+    }
 }
